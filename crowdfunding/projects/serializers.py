a0ec25df2a5c43c3249fbe6fe82213100e81c1eb
Julie E Powell <JEPowell@proton.me>
2023-07-30 10:34:59 +1000

    first crowdfunding commit

---
+++ crowdfunding/projects/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from django.apps import apps
+
+class PledgeSerializer(serializers.ModelSerializer):
+class Meta:
+    model = apps.get_model('projects.Pledge')
+    fields = '__all__'
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:        
+        model = apps.get_model('projects.Project')        
+        fields ='__all__'
+
