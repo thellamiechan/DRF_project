9bb82122e31fef3a52d0c9b59eb4c2f4277ad372
Julie E Powell <JEPowell@proton.me>
2023-08-13 12:51:25 +1000

    added delete

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -64,6 +64,12 @@ class ProjectDetail(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
     
+    def delete(self, request, pk, format=None):
+        project = self.get_object(pk)
+        project.delete()
+        return Response(status=status.HTTP_200_OK)
+    
+
 class PledgeList(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
